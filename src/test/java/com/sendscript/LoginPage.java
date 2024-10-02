b967de9ba3bd427619784aa9837fc612fc201b95
Maneesha N <maneeshamanu53@gmail.com>
2024-10-02 15:54:35 +0530

    First commit.

---
+++ src/test/java/com/sendscript/LoginPage.java
@@ -0,0 +1,18 @@
+package com.sendscript;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class LoginPage 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Page Title is: ");
+    }
+}
